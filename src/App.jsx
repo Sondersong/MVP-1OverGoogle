461a89d77309e3e4d8ce8c2d9411a7fd32f9fc4b
Sondersong <brianlgage@gmail.com>
2023-06-06 12:40:00 -0600

    feat: setup scaling image grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./css/App.css";
 import { images } from "./imgImport"
 function App() {
-  const [gridScale, setGridScale] = useState([false, 25]);
+  const [gridScale, setGridScale] = useState([false, 50]);
   let imgColumns = [];
   function assignPhotos() {
     let currentColumn = 0;
@@ -19,6 +19,15 @@ function App() {
   let column1 = imgColumns.filter(element => element[1] === 1);
   let column2 = imgColumns.filter(element => element[1] === 2);
   let column3 = imgColumns.filter(element => element[1] === 3);
+
+  function changeScale(e) {
+    let newScale = Number(e.target.value)
+    if (newScale === 50) {
+      setGridScale([true, 100])
+    } else {
+      setGridScale([true, newScale + 25])
+    }
+  }
   return (
     <div className="App">
       <div id="titleBar">
@@ -28,7 +37,7 @@ function App() {
       </div>
       <div id="scaleBar">
         <form>
-          <label htmlFor="scale">- Grid Size -</label>
+          <label htmlFor="scale">- Photo Size -</label>
           <input
             type="range"
             name="scale"
@@ -36,28 +45,29 @@ function App() {
             min="0"
             max="50"
             step="25"
-            defaultValue={gridScale[1]}
+            defaultValue="25"
+            onChange={changeScale}
           />
         </form>
       </div>
       <div id="photoGrid">
-        <div className="column">
+        <div className="column" style={{flex: `${gridScale[1]}%`}}>
           {column0.map(element => {
             return <img key={element[0]} src={images[`img${element[0]}`]}></img>
           })}
         </div>
-        <div className="column">
+        <div className="column" style={{flex: `${gridScale[1]}%`}}>
+          {column3.map(element => {
+            return <img key={element[0]} src={images[`img${element[0]}`]}></img>
+          })}
+        </div>
+        <div className="column" style={{flex: `${gridScale[1]}%`}}>
           {column1.map(element => {
             return <img key={element[0]} src={images[`img${element[0]}`]}></img>
           })}
         </div>
-        <div className="column">
+        <div className="column" style={{flex: `${gridScale[1]}%`}}>
           {column2.map(element => {
-            return <img key={element[0]} src={images[`img${element[0]}`]}></img>
-          })}
-        </div>
-        <div className="column">
-          {column3.map(element => {
             return <img key={element[0]} src={images[`img${element[0]}`]}></img>
           })}
         </div>
